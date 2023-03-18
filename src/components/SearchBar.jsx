2d646140cf96dee26e8f1d52f473c4362dee3723
Tiaanz <jwtian126@gmail.com>
2023-03-18 13:13:30 +1300

    deleted extra lines

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -12,33 +12,6 @@ const SearchBar = ({ searchPlace, setSearchPlace, searchWeather }) => {
     searchWeather(searchPlace)
   }
 
-  // function handleChange(e) {
-  //   setSearchPlace(e.target.value)
-  //   if (e.target.value === '') {
-  //     setSearchResult(() => initialresult)
-  //   }
-
-  //   //get search results array
-  //   async function fetchData() {
-  //     try {
-  //       if (e.target.value) {
-  //         const response = await fetch(`${API_URL}?q=${e.target.value}`, {
-  //           method: 'GET',
-  //           headers: {
-  //             'X-RapidAPI-Key':
-  //               '6cda750ddbmsh3e2d299b52be602p1f7255jsn589c028d6d61',
-  //             'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com',
-  //           },
-  //         })
-  //         const result = await response.json()
-  //         setSearchResult(() => result)
-  //       }
-  //     } catch (error) {
-  //       console.log(error.message)
-  //     }
-  //   }
-  //   fetchData()
-  // }
 
   const formatResult = (item) => {
     return (
@@ -56,10 +29,6 @@ const SearchBar = ({ searchPlace, setSearchPlace, searchWeather }) => {
     } else {
       setSearchPlace('')
     }
-
-    // if (string === '') {
-    //   setSearchResult(() => initialresult)
-    // }
 
     //get search results array
     async function fetchData() {
@@ -88,18 +57,9 @@ const SearchBar = ({ searchPlace, setSearchPlace, searchWeather }) => {
   }
 
 
-
   return (
     <div className="w-3/4 sm:w-3/5 md:w-2/4  lg:w-2/5">
       <form className="w-full" onSubmit={SubmitHandler}>
-        {/* <div className=" w-3/4 sm:w-3/5 md:w-2/4  lg:w-2/5 flex items-center relative"> */}
-        {/* <input
-            className=" h-12 px-5 pr-10 rounded-full w-full text-lg sm:text-xl focus:outline-none shadow-sm"
-            placeholder="Enter your city"
-            type="text"
-            value={searchPlace}
-            onChange={handleChange} />*/}
-
         <ReactSearchAutocomplete
           items={searchResult}
           onSearch={handleOnSearch}
@@ -109,24 +69,6 @@ const SearchBar = ({ searchPlace, setSearchPlace, searchWeather }) => {
           autoFocus
           styling={{fontSize:'18px', border:'none'}}
         />
-
-        {/* // <button type="submit" className="absolute right-3">
-          //   <svg */}
-        {/* xmlns="http://www.w3.org/2000/svg" */}
-        {/* //     fill="none" */}
-        {/* //     viewBox="0 0 24 24" */}
-        {/* //     strokeWidth={1.5} */}
-        {/* //     stroke="currentColor" */}
-        {/* //     className="w-8 h-8 text-slate-300" */}
-        {/* //   > */}
-        {/* //     <path */}
-        {/* //       strokeLinecap="round" */}
-        {/* //       strokeLinejoin="round" */}
-        {/* //       d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" */}
-        {/* //     /> */}
-        {/* //   </svg> */}
-        {/* // </button> */}
-        {/* // </div> */}
       </form>
     </div>
   )
