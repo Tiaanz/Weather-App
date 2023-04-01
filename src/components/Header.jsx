6763e763b2e6a6c0673da4cc50fe3973f54e8f87
Tiaanz <jwtian126@gmail.com>
2023-04-01 22:44:12 +1300

    fixed something

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <h1 className="text-3xl sm:text-4xl my-20 font-header text-center">
-      What's the weather today?
+      What's the weather like today?
     </h1>
   )
 }
